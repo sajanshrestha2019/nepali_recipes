dd1091f69df7503123fdaf88538600507709d69a
Sajan Shrestha <sajanshrestharm11@gmail.com>
2020-11-30 14:20:35 -0500

    Nepali Cuisine project started

---
+++ NepaliCuisine/NepaliCuisineApp.swift
@@ -0,0 +1,17 @@
+//
+//  NepaliCuisineApp.swift
+//  NepaliCuisine
+//
+//  Created by Sajan Shrestha on 11/30/20.
+//
+
+import SwiftUI
+
+@main
+struct NepaliCuisineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
